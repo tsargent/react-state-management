5a5dec0a72094301e714f8029107931f89b0704a
Tyler Sargent <tsargent@gmail.com>
2020-08-06 11:15:11 -0400

    create todos context page to begin refactor to context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import TodosPage from "./screens/Todos";
+import TodosReducerPage from "./screens/TodosReducer";
+import TodosContextPage from "./screens/TodosContext";
 import HomePage from "./screens/Home";
 import "./App.css";
 
@@ -10,10 +11,12 @@ const App = () => {
       <nav className="nav">
         <Link to="/" className="nav-item">Home</Link>
         <Link to="/todos-reducer" className="nav-item">Todos Reducer</Link>
+        <Link to="/todos-context" className="nav-item">Todos Context</Link>
       </nav>
       <main className="main">
         <Routes>
-          <Route path="/todos-reducer" element={<TodosPage />} />
+          <Route path="/todos-reducer" element={<TodosReducerPage />} />
+          <Route path="/todos-context" element={<TodosContextPage />} />
           <Route path="/" element={<HomePage />} />
         </Routes>
       </main>
---
+++ src/screens/TodosReducer.js
@@ -0,0 +1,58 @@
+import React, { useCallback, useReducer } from "react";
+import Todos from "../Todos";
+import NewTodo from "../NewTodo";
+import initialState from "../initialState";
+
+const ADD_TODO = "ADD_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [action.payload, ...state];
+    case TOGGLE_TODO:
+      return state.map((todo) => {
+        if (todo.id !== action.payload) {
+          return todo;
+        } else {
+          return { ...todo, complete: !todo.complete };
+        }
+      });
+    default:
+      return state;
+  }
+};
+
+const TodosScreen = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const addTodo = useCallback(
+    (text) => {
+      dispatch({
+        type: ADD_TODO,
+        payload: text,
+      });
+    },
+    [dispatch]
+  );
+
+  const toggleTodo = useCallback(
+    (id) => {
+      dispatch({
+        type: TOGGLE_TODO,
+        payload: id,
+      });
+    },
+    [dispatch]
+  );
+
+  return (
+    <div className="App">
+      <h1>Todos</h1>
+      <NewTodo addTodo={addTodo} />
+      <Todos todos={state} toggleTodo={toggleTodo} />
+    </div>
+  );
+};
+
+export default TodosScreen;
