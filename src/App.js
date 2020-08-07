a6d17405d555273d4107a8a40c6103789db41e6d
Tyler Sargent <tsargent@gmail.com>
2020-08-06 22:13:18 -0400

    wrap page element in provider

--- src/App.js
+++ src/App.js
@@ -6,25 +6,28 @@ import HomePage from "./screens/Home";
 import { TodosProvider } from "./todos-context/TodosContext";
 import "./App.css";
 
+const TodosContextElement = () => (
+  <TodosProvider>
+    <TodosContextPage />
+  </TodosProvider>
+);
 
 const App = () => {
   return (
-    <TodosProvider>
-      <BrowserRouter>
-        <nav className="nav">
-          <Link to="/" className="nav-item">Home</Link>
-          <Link to="/todos-reducer" className="nav-item">Todos Reducer</Link>
-          <Link to="/todos-context" className="nav-item">Todos Context</Link>
-        </nav>
-        <main className="main">
-          <Routes>
-            <Route path="/todos-reducer" element={<TodosReducerPage />} />
-            <Route path="/todos-context" element={<TodosContextPage />} />
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-        </main>
-      </BrowserRouter>
-    </TodosProvider>
+    <BrowserRouter>
+      <nav className="nav">
+        <Link to="/" className="nav-item">Home</Link>
+        <Link to="/todos-reducer" className="nav-item">Todos Reducer</Link>
+        <Link to="/todos-context" className="nav-item">Todos Context</Link>
+      </nav>
+      <main className="main">
+        <Routes>
+          <Route path="/todos-reducer" element={<TodosReducerPage />} />
+          <Route path="/todos-context" element={<TodosContextElement />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 };
 
