ee487415d284cf216439ac66fdbd0cff689ed716
Tyler Sargent <tsargent@gmail.com>
2020-08-11 09:48:04 -0400

    change api

--- src/screens/StarWars.js
+++ src/screens/StarWars.js
@@ -2,13 +2,13 @@ import React from "react";
 import useFetch from '../hooks/useFetch';
 
 const StarWarsScreen = () => {
-  const {isLoading, isError, response} = useFetch('http://swapi.dev/api/people');
+  const {isLoading, isError, response} = useFetch('https://star-wars-character-search.glitch.me/api/characters');
   return (
     <div className="App">
       <h1>Star Wars</h1>
       {isLoading ? <p>Loading characters...</p> : ''}
       {isError ? <p>Error!</p> : ''}
-      {response?.data.results.map((character) => <p key={character.name}>{character.name}</p>)}
+      {response?.data.characters.map((character) => <p key={character.name}>{character.name}</p>)}
     </div>
   );
 };
