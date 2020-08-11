9ec578b6fa46c8799bd1f8ff7b0cd49a9a5fae19
Tyler Sargent <tsargent@gmail.com>
2020-08-10 22:40:25 -0400

    list characters

--- src/screens/StarWars.js
+++ src/screens/StarWars.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const StarWarsScreen = () => {
+  const [characters, setCharacters] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false);
+  useEffect(() => {
+    setIsLoading(true)
+    axios.get('https://swapi.dev/api/people')
+      .then((response) => {
+        setIsLoading(false)
+        setCharacters(response.data.results)
+      })
+      .catch((error) => {
+        setIsError(true)
+      })
+  }, [])
   return (
     <div className="App">
       <h1>Star Wars</h1>
+      {isLoading ? <p>Loading characters...</p> : ''}
+      {isError ? <p>Error!</p> : ''}
+      {characters.map((character) => <p key={character.name}>{character.name}</p>)}
     </div>
   );
 };
